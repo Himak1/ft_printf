c7b4ca9626e4abf50b67bc1cde3b466e3bb62cf3
jhille <jhille@student.codam.nl>
2021-02-19 14:10:56 +0000

    added test.c

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: jhille <marvin@codam.nl>                     +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/14 21:06:09 by jhille        #+#    #+#                 */
-/*   Updated: 2021/02/16 11:19:55 by jhille        ########   odam.nl         */
+/*   Updated: 2021/02/19 14:10:47 by jhille        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int main()
 {
-/*
+
 	int		a = -4;
 	int		b = 0;
 	char	c = 'a';
@@ -39,8 +39,11 @@ int main()
 	char	*t = "0x12345678";
 	char	*u = "-0";
 
+	ft_printf("%0-i, %0-d, %0-d, %0-d, %0-d, %0-d, %0-d, %0-d", i, j, k, l, m, c, e, d); //T16
+/*
 	printf(" --- Return : %d\n", ft_printf("%p, %i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X", n, i, j, k, l, m, n, c, c, j, j, j)); //T2
 	printf(" --- Return : %d\n", printf("%p, %i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X", n, i, j, k, l, m, n, c, c, j, j, j)); //T2
-*/
+
 	printf("%d", printf("%100000000000000000d", 12));
+*/
 }
